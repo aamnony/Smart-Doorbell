9c2da79153bd2e857915815717407054743f7825
Asaf Amnony <aamnony@gmail.com>
2019-03-25 21:37:50 +0200

    Moved STREAM_SERVER_URL to AppHelper and fcm.json path

--- android/app/src/main/java/com/github/aamnony/smartdoorbell/StreamActivity.java
+++ android/app/src/main/java/com/github/aamnony/smartdoorbell/StreamActivity.java
@@ -8,16 +8,16 @@ import org.jitsi.meet.sdk.JitsiMeetView;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import com.github.aamnony.smartdoorbell.AppHelper;
+
 public class StreamActivity extends JitsiMeetActivity {
 
     public static final String EXTRA_USER_NAME = "user_name";
-    private static final String STREAM_SERVER_URL = "https://www.smartdoorbell.ga/";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        String roomUrl = STREAM_SERVER_URL + getIntent().getStringExtra(EXTRA_USER_NAME) +
+        String roomUrl = AppHelper.STREAM_SERVER_URL + getIntent().getStringExtra(EXTRA_USER_NAME) +
                 "#config.startWithVideoMuted=true" + "&config.startWithAudioMuted=true";
         try {
             loadURL(new URL(roomUrl));
